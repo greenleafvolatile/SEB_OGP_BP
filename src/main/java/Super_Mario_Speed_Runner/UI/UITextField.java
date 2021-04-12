9327616afa9f76512fd5f15b1c5eb629f935f0f5
greenleavolatile <daanpol@tutamail.com>
2021-04-12 08:42:56 +0200

    Added UITextField

---
+++ src/main/java/Super_Mario_Speed_Runner/UI/UITextField.java
@@ -0,0 +1,21 @@
+package Super_Mario_Speed_Runner.UI;
+
+import processing.core.PApplet;
+
+public class UITextField extends UIComponent {
+
+    private float fieldWidth, fieldHeight;
+
+    public UITextField(float xPos, float yPos, float fieldWidth, float fieldHeight) {
+
+        super(xPos, yPos);
+        this.fieldWidth = fieldWidth;
+        this.fieldHeight = fieldHeight;
+
+    }
+
+    public void draw(PApplet app) {
+
+
+    }
+}
