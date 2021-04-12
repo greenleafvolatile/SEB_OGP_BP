097aed627bfb9ba068a96a262649aa7bbe9a41d9
greenleavolatile <daanpol@tutamail.com>
2021-04-12 08:48:50 +0200

    Added UITextField

---
+++ src/main/java/Super_Mario_Speed_Runner/UI/UICaret.java
@@ -0,0 +1,19 @@
+package Super_Mario_Speed_Runner.UI;
+
+import processing.core.PApplet;
+
+public final class UICaret extends UIComponent {
+
+    private float xPos, yPos;
+
+    public UICaret(float xPos, float yPos) {
+        super(xPos, yPos);
+
+    }
+
+
+    @Override
+    public void draw(PApplet app) {
+
+    }
+}
--- src/main/java/Super_Mario_Speed_Runner/UI/UITextField.java
+++ src/main/java/Super_Mario_Speed_Runner/UI/UITextField.java
@@ -2,18 +2,24 @@ package Super_Mario_Speed_Runner.UI;
 
 import processing.core.PApplet;
 
-public class UITextField extends UIComponent {
+
+
+public final class UITextField extends UIComponent {
 
     private float fieldWidth, fieldHeight;
+    private UICaret caret;
 
     public UITextField(float xPos, float yPos, float fieldWidth, float fieldHeight) {
 
         super(xPos, yPos);
+        this.caret = new UICaret(xPos, yPos);
         this.fieldWidth = fieldWidth;
         this.fieldHeight = fieldHeight;
 
+
     }
 
+    @Override
     public void draw(PApplet app) {
 
 
