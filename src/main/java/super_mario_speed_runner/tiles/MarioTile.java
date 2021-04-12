d53dbbd473cc728a133a88df8ebaab0d276466bd
greenleavolatile <daanpol@tutamail.com>
2021-04-12 09:22:50 +0200

    Added boilerplate for tile.

---
+++ src/main/java/super_mario_speed_runner/tiles/MarioTile.java
@@ -0,0 +1,11 @@
+package super_mario_speed_runner.tiles;
+
+import nl.han.ica.oopg.objects.Sprite;
+import nl.han.ica.oopg.tile.Tile;
+
+public class MarioTile extends Tile {
+
+    public MarioTile (Sprite sprite) {
+        super(sprite);
+    }
+}
