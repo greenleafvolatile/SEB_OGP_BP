4d554e1de1bb3b4bb896484bf2e53edee5f5f4f9
greenleavolatile <daanpol@tutamail.com>
2021-04-12 08:53:31 +0200

    Added boilerplate for tiles.

---
+++ src/main/java/Super_Mario_Speed_Runner/Tiles/DoorTile.java
@@ -0,0 +1,15 @@
+package Super_Mario_Speed_Runner.Tiles;
+
+import nl.han.ica.oopg.objects.Sprite;
+import nl.han.ica.oopg.objects.SpriteObject;
+import nl.han.ica.oopg.tile.Tile;
+
+import java.io.DataOutputStream;
+
+public final class DoorTile extends Tile {
+
+    public DoorTile(Sprite sprite) {
+        super(sprite);
+    }
+
+}
---
+++ src/main/java/Super_Mario_Speed_Runner/Tiles/GroundTile.java
@@ -0,0 +1,11 @@
+package Super_Mario_Speed_Runner.Tiles;
+
+import nl.han.ica.oopg.objects.Sprite;
+import nl.han.ica.oopg.tile.Tile;
+
+public final class GroundTile extends Tile {
+
+    public GroundTile(Sprite sprite) {
+        super(sprite);
+    }
+}
