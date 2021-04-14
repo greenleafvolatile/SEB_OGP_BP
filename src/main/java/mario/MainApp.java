9f39def1fda32c720ff1296e3b787a82db84f2a1
greenleavolatile <daanpol@tutamail.com>
2021-04-14 14:10:43 +0200

    Added highscore.txt for testing purposes.

--- src/main/java/mario/MainApp.java
+++ src/main/java/mario/MainApp.java
@@ -2,6 +2,7 @@ package mario;
 
 import nl.han.ica.oopg.engine.GameEngine;
 import nl.han.ica.oopg.objects.Sprite;
+import nl.han.ica.oopg.tile.Tile;
 import nl.han.ica.oopg.tile.TileMap;
 import nl.han.ica.oopg.tile.TileType;
 import nl.han.ica.oopg.view.View;
