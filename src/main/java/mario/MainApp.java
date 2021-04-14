d5ca8da432c6c6637af065e040630c759be66e27
greenleavolatile <daanpol@tutamail.com>
2021-04-14 15:47:58 +0200

    Added keyPressed keyReleased to Player. Added viewPort

--- src/main/java/mario/MainApp.java
+++ src/main/java/mario/MainApp.java
@@ -15,6 +15,7 @@ public class MainApp extends GameEngine {
 
     private Player player;
 
+
     public static void main(String[] args) {
         MainApp app = new MainApp();
 
@@ -24,19 +25,34 @@ public class MainApp extends GameEngine {
 
     @Override
     public void setupGame() {
-        int screenWidth = 1024;
-        int screenHeight = 768;
+        int screenWidth = 1204;
+        int screenHeight = 640;
 
         size(screenWidth, screenHeight);
-
-        initializeTileMap();
         createGameObjects();
 
-        //CenterFollowingViewport centerFollowingViewport = new CenterFollowingViewport(player, screenWidth, screenHeight, 0, 0);
+        createViewCenterViewport(6400, 640, screenWidth, screenHeight);
+        initializeTileMap();
 
-        View view = new View(screenWidth, screenHeight);
-        //view.setViewport(centerFollowingViewport);
+
+    }
+
+    /**
+     * Create view with center viewport
+     *
+     * @param worldWidth   Game level total width
+     * @param worldHeight  Game level total Height
+     * @param screenWidth  Total width screen
+     * @param screenHeight Total height screen
+     *
+     * Based on code from Waterworld
+     */
+    private void createViewCenterViewport(int worldWidth, int worldHeight, int screenWidth, int screenHeight) {
+        CenterFollowingViewport viewPort = new CenterFollowingViewport(player, (int) Math.ceil(screenWidth), (int) Math.ceil(screenHeight), 0, 100);
+        viewPort.setTolerance(50, 50, 50, 50);
+        View view = new View(viewPort, worldWidth, worldHeight);
         setView(view);
+        size(screenWidth, screenHeight);
     }
 
     @Override
@@ -45,9 +61,8 @@ public class MainApp extends GameEngine {
     }
 
     public void createGameObjects() {
-
         player = new Player(this);
-        addGameObject(player, 5);
+        addGameObject(player, 0, 481);
     }
 
 
@@ -75,6 +90,7 @@ public class MainApp extends GameEngine {
         TileType<FloorTile> noLockDoorTopTileType = new TileType<>(FloorTile.class, noLockDoorTopSprite); // 6
         TileType<FloorTile> platformBlueTileType = new TileType<>(FloorTile.class, platformBlueSprite); // 7
 
+
         TileType[] tileTypes = {
                 boxTileType,
                 groundTileType,
@@ -87,10 +103,8 @@ public class MainApp extends GameEngine {
         };
 
 
-        int tileSize = 32;
+        int tileSize = 64;
         int[][] tilesMap = MapLoader.loadMap();
         tileMap = new TileMap(tileSize, tileTypes, tilesMap);
     }
-
-
 }
--- src/main/java/mario/Player.java
+++ src/main/java/mario/Player.java
@@ -8,34 +8,44 @@ import nl.han.ica.oopg.exceptions.TileNotFoundException;
 import nl.han.ica.oopg.objects.AnimatedSpriteObject;
 import nl.han.ica.oopg.objects.GameObject;
 import nl.han.ica.oopg.objects.Sprite;
-import nl.han.ica.oopg.sound.Sound;
 import processing.core.PVector;
-import tutorials.TutorialWorld;
 
 import java.util.List;
-import java.util.logging.Logger;
 
 public class Player extends AnimatedSpriteObject implements ICollidableWithTiles, ICollidableWithGameObjects {
 
-    private final MainApp app;
-    private final Sound jumpSound;
-
-    private int speed = 1;
-    private float gravity;
+    private MainApp app;
 
     public Player(MainApp app) {
-
         super(new Sprite(MainApp.MEDIA_URL.concat("sprites/characters/mario.png")), 7);
         this.app = app;
-        this.jumpSound = new Sound(app, MainApp.MEDIA_URL.concat("sounds/jump_11.wav"));
-        this.gravity = 0.6f;
-        this.initPlayer();
+
+        final float gravity = 0.2f;
+        this.setGravity(gravity);
     }
 
-    private void initPlayer() {
+    @Override
+    public void keyPressed(int keyCode, char key) {
+        final int speed = 5;
+        if (keyCode == app.RIGHT) {
+            setDirectionSpeed(90, speed);
+        } else if (keyCode == app.LEFT) {
+            setDirectionSpeed(270, speed);
+        } else if (key == ' ') {
+            setDirectionSpeed(360, 5);
+        }
+    }
 
-        this.setGravity(gravity);
-        this.setSpeed(speed);
+    @Override
+    public void keyReleased(int keyCode, char key) {
+        super.keyReleased(keyCode, key);
+        if (keyCode == app.RIGHT) {
+            setDirectionSpeed(0, 0);
+        } else if (keyCode == app.LEFT) {
+            setDirectionSpeed(0, 0);
+        } else if (key == ' ') {
+            setDirectionSpeed(0, 0);
+        }
     }
 
     @Override
@@ -52,44 +62,20 @@ public class Player extends AnimatedSpriteObject implements ICollidableWithTiles
             if (tile.getTile() instanceof FloorTile) {
 
                 try {
-                    vector = this.app.getTileMap().getTilePixelLocation(tile.getTile());
-                    this.setY(vector.y - getHeight());
+                    vector = app.getTileMap().getTilePixelLocation(tile.getTile());
+                    setY(vector.y - getHeight());
 
                 } catch (TileNotFoundException e) {
                     e.printStackTrace();
                 }
+
+                setySpeed(0);
             }
         }
     }
 
     @Override
     public void update() {
-        //System.out.println("Speed: " + this.speed);
-        //System.out.println("Gravity: " +  this.gravity);
-        //System.out.println("yPos: " + this.getY());
 
-    }
-
-    @Override
-    public void keyPressed(int keyCode, char key) {
-
-        if (keyCode == TutorialWorld.RIGHT) {
-            setDirectionSpeed(90, speed);
-        }
-
-        if (keyCode == TutorialWorld.LEFT) {
-            setDirectionSpeed(270, speed);
-        }
-
-        if (key == ' ') {
-            setDirectionSpeed(360, 5);
-            this.jumpSound.cue(0);
-            this.jumpSound.play();
-        }
-    }
-
-    @Override
-    public void keyReleased(int keyCode, char key) {
-        this.setSpeed(0);
     }
 }
