14b1e124b760d9f5c77f437b910884071bf3d7e0
Christiaan Wiggers <christiaan_wiggers@yahoo.com>
2021-04-15 12:17:08 +0200

    Added key tile to map

--- src/main/java/mario/Map.java
+++ src/main/java/mario/Map.java
@@ -1,6 +1,8 @@
 package mario;
 
 import mario.tiles.FloorTile;
+import mario.tiles.KeyTile;
+import mario.tiles.LavaTile;
 import mario.tiles.MarioTile;
 import nl.han.ica.oopg.objects.Sprite;
 import nl.han.ica.oopg.tile.TileType;
@@ -36,15 +38,17 @@ public class Map {
         Sprite noLockDoorBottomSprite = new Sprite(MainApp.MEDIA_URL + "sprites/ground/no_lock_door_bottom.png"); // 5
         Sprite noLockDoorTopSprite = new Sprite(MainApp.MEDIA_URL + "sprites/ground/no_lock_door_top.png"); // 6
         Sprite platformBlueSprite = new Sprite(MainApp.MEDIA_URL + "sprites/ground/platform_blue.png"); // 7
+        Sprite keySprite = new Sprite(MainApp.MEDIA_URL + "sprites/items/keyYellow.png"); // 8
 
         TileType<FloorTile> boxTileType = new TileType<>(FloorTile.class, boxSprite); // 0
         TileType<FloorTile> groundTileType = new TileType<>(FloorTile.class, groundSprite); // 1
-        TileType<FloorTile> lavaTileType = new TileType<>(FloorTile.class, lavaSprite); // 2
+        TileType<LavaTile> lavaTileType = new TileType<>(LavaTile.class, lavaSprite); // 2
         TileType<FloorTile> doorYellowTopTileType = new TileType<>(FloorTile.class, doorYellowTopSprite); // 3
         TileType<FloorTile> doorYellowBottomTileType = new TileType<>(FloorTile.class, doorYellowBottomSprite); // 4
         TileType<FloorTile> noLockDoorBottomTileType = new TileType<>(FloorTile.class, noLockDoorBottomSprite); // 5
         TileType<FloorTile> noLockDoorTopTileType = new TileType<>(FloorTile.class, noLockDoorTopSprite); // 6
         TileType<FloorTile> platformBlueTileType = new TileType<>(FloorTile.class, platformBlueSprite); // 7
+        TileType<KeyTile> keyTileType = new TileType<>(KeyTile.class, keySprite); // 8
 
 
         this.tileTypes = new TileType[]{
@@ -55,7 +59,8 @@ public class Map {
                 doorYellowBottomTileType,
                 noLockDoorBottomTileType,
                 noLockDoorTopTileType,
-                platformBlueTileType
+                platformBlueTileType,
+                keyTileType
         };
 
     }
