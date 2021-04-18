2ebf5cc5ae15a5e077ade883f7664764922f0377
PiDexter <christiaan_wiggers@yahoo.com>
2021-04-18 11:06:49 +0200

    Add view with viewport to Game

--- src/main/java/mario/Game.java
+++ src/main/java/mario/Game.java
@@ -2,6 +2,8 @@ package mario;
 
 import nl.han.ica.oopg.tile.TileMap;
 import nl.han.ica.oopg.tile.TileType;
+import nl.han.ica.oopg.view.CenterFollowingViewport;
+import nl.han.ica.oopg.view.View;
 
 import java.io.File;
 
@@ -11,17 +13,20 @@ public class Game {
 
     private MainApp app;
     private Player player;
+    private TileMap tileMap;
 
     private final File[] mapFiles = { new File(MainApp.MEDIA_URL.concat("maps/lvl1.csv"))};
 
     public Game(MainApp app) {
         this.app = app;
-        this.app.setTileMap(initMap());
+        this.tileMap = initMap();
+        this.app.setTileMap(tileMap);
     }
 
     public void display() {
         createObjects();
         initMap();
+        createView();
     }
 
     private void createObjects() {
@@ -33,5 +38,22 @@ public class Game {
         TileType[] tileTypes =  loadTileTypes();
         int[][] tilesMap = MapLoader.loadMap(mapFiles[0]);
         return new TileMap(64, tileTypes, tilesMap);
+    }
+
+    private CenterFollowingViewport centerViewport() {
+        int screenWidth = this.app.getWidth();
+        int screenHeight = this.app.getHeight();
+
+        CenterFollowingViewport viewPort = new CenterFollowingViewport(player, screenWidth, screenHeight, 0, 100);
+        viewPort.setTolerance(50, 0, 50, 50);
+        return viewPort;
+    }
+
+    private void createView() {
+        int worldWidth = this.tileMap.getMapWidth();
+        int worldHeight = this.tileMap.getMapHeight();
+
+        View view = new View(centerViewport(), worldWidth, worldHeight);
+        this.app.setView(view);
     }
 }
