7045e9840a97b1cfd782d2df751f2f48812feac5
PiDexter <christiaan_wiggers@yahoo.com>
2021-04-22 20:46:27 +0200

    Update plant sprite

--- src/main/java/mario/enemies/PiranhasPlant.java
+++ src/main/java/mario/enemies/PiranhasPlant.java
@@ -5,7 +5,7 @@ import nl.han.ica.oopg.objects.Sprite;
 
 public class PiranhasPlant extends Enemy {
 
-    public PiranhasPlant(MainApp app, Sprite sprite, int totalFrames) {
-        super(app, new Sprite(MainApp.MEDIA_URL.concat("sprites/characters/plant.png")), 62);
+    public PiranhasPlant(MainApp app) {
+        super(app, new Sprite(MainApp.MEDIA_URL.concat("sprites/characters/plant.png")), 176);
     }
 }
