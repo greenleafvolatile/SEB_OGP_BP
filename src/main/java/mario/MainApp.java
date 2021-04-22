40561d6b023c91d356703556e7bfe592e184d52a
greenleavolatile <daanpol@tutamail.com>
2021-04-22 10:48:07 +0200

    Messing around

--- src/main/java/mario/MainApp.java
+++ src/main/java/mario/MainApp.java
@@ -1,6 +1,7 @@
 package mario;
 
 import nl.han.ica.oopg.engine.GameEngine;
+import nl.han.ica.oopg.persistence.FilePersistence;
 
 public class MainApp extends GameEngine {
 
@@ -20,6 +21,22 @@ public class MainApp extends GameEngine {
         int screenHeight = 768;
 
         size(screenWidth, screenHeight);
+
+        FilePersistence filePersistence = new FilePersistence("/main/java/mario/media/highscores/highscores.txt");
+        String[] data = filePersistence.loadDataStringArray("\n");
+        System.out.println(data.length);
+        for (String string : data) {
+            String[] split = string.split(" ");
+            String name = split[0];
+            String time = split[1];
+
+
+            System.out.println(name);
+
+            System.out.println(time);
+        }
+        String newEntry = "Piet "
+
 
         new StateManager(this);
     }
