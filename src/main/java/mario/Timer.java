705157615f046af847a48aeccb83743df81e682c
Christiaan Wiggers <christiaan_wiggers@yahoo.com>
2021-04-23 08:26:11 +0200

    Refactor timer and implement in dashboard

--- src/main/java/mario/Timer.java
+++ src/main/java/mario/Timer.java
@@ -4,64 +4,47 @@ import java.util.concurrent.TimeUnit;
 
 public class Timer {
 
-    private long start = System.nanoTime();
-    private long end;
+    private double start;
+    private double end;
+    private boolean active = false;
 
-    private long start2;
-
-    private MainApp app;
+    private final MainApp app;
 
     public Timer(MainApp app) {
         this.app = app;
-        this.start2 = 0;
+        this.start = app.millis();
     }
 
-    private boolean active = false;
-
     public void start() {
-        this.start = System.nanoTime();
-        this.start2 = this.app.millis();
+        this.start = this.app.millis();
         this.active = true;
     }
 
     public void end() {
-        this.end = System.nanoTime();
+        this.end = getSeconds();
         this.active = false;
     }
 
-    public long getElapsedTimeInSeconds() { ;
-        return System.nanoTime() - this.start;
-    }
-
     public double getSeconds() {
-        return (double) this.start2 / 1000;
+        return (this.app.millis() - this.start) / 1000;
     }
 
-    public double inSeconds() {
-        return (double) TimeUnit.SECONDS.convert(getElapsedTimeInSeconds(), TimeUnit.NANOSECONDS);
-    }
-
-    public String getFormatedTime2() {
+    public String getFormatedTime() {
         int secs = (int) getSeconds();
         return String.format("%02d:%02d:%02d", secs / 3600, (secs % 3600) / 60, secs % 60);
     }
 
-    public String getFormatedTime() {
-        int secs = (int) inSeconds();
+    public double getElapsedSeconds() {
+        return this.end;
+    }
+
+    public String getFormatedFinalTime() {
+        int secs = (int) getElapsedSeconds();
         return String.format("%02d:%02d:%02d", secs / 3600, (secs % 3600) / 60, secs % 60);
     }
 
     public boolean isActive() {
         return active;
     }
-
-//        public static void main(String[] args) {
-//        Timer time = new Timer();
-//        time.start();
-//
-//        while(time.active) {
-//            System.out.println(time.getFormatedTime());
-//        }
-//
-//    }
 }
+
