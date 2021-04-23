6a79b1cd28191c1f83cbb0ad6b9c642130ccf632
Christiaan Wiggers <info@qwans.nl>
2021-04-23 08:18:58 +0200

    Added final time method

--- src/main/java/mario/Timer.java
+++ src/main/java/mario/Timer.java
@@ -4,8 +4,8 @@ import java.util.concurrent.TimeUnit;
 
 public class Timer {
 
-    private long start;
-    private long end;
+    private double start;
+    private double end;
 
     private MainApp app;
 
@@ -22,16 +22,25 @@ public class Timer {
     }
 
     public void end() {
-        this.end = this.app.millis();
+        this.end = getSeconds();
         this.active = false;
     }
 
     public double getSeconds() {
-        return (double) (this.app.millis() - this.start) / 1000;
+        return (this.app.millis() - this.start) / 1000;
     }
 
     public String getFormatedTime() {
         int secs = (int) getSeconds();
+        return String.format("%02d:%02d:%02d", secs / 3600, (secs % 3600) / 60, secs % 60);
+    }
+
+    public double getElapsedSeconds() {
+        return this.end;
+    }
+
+    public String getFormatedFinalTime() {
+        int secs = (int) getElapsedSeconds();
         return String.format("%02d:%02d:%02d", secs / 3600, (secs % 3600) / 60, secs % 60);
     }
 
