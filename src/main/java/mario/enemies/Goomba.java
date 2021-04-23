cca070a88844b729fe3fd8198d3c30f987697156
Christiaan Wiggers <christiaan_wiggers@yahoo.com>
2021-04-23 10:38:42 +0200

    Add javadoc blocks to enemy classes

--- src/main/java/mario/enemies/Goomba.java
+++ src/main/java/mario/enemies/Goomba.java
@@ -3,8 +3,16 @@ package mario.enemies;
 import mario.MainApp;
 import nl.han.ica.oopg.objects.Sprite;
 
+/**
+ * The type Goomba.
+ */
 public class Goomba extends Enemy {
 
+    /**
+     * Instantiates a new Goomba.
+     *
+     * @param app the app
+     */
     public Goomba(MainApp app) {
         super(app, new Sprite(MainApp.MEDIA_URL.concat("sprites/characters/goomba.png")), 8);
         setxSpeed(5);
