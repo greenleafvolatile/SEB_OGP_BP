669dca66b15b77b6f6caaf7401aa31eef0fe0753
PiDexter <christiaan_wiggers@yahoo.com>
2021-04-24 11:50:47 +0200

    Add toString method, returns time

--- src/main/java/mario/Timer.java
+++ src/main/java/mario/Timer.java
@@ -26,6 +26,9 @@ public class Timer {
         this.start = this.app.millis();
     }
 
+    /**
+     * Stop timer.
+     */
     public void stopTimer() {
         this.end = this.app.millis();
     }
@@ -44,14 +47,12 @@ public class Timer {
         return (this.app.millis() - this.start) / 1000f;
     }
 
-    /*
-     * Gets formatted time.
-     *
-     * @return the formatted time
-     *
-    public String getFormattedTime() {
+
+    @Override
+    public String toString() {
         int secs = (int) getElapsedTime();
         return String.format("%02d:%02d", (secs % 3600) / 60, secs % 60);
-    }*/
+    }
+
 }
 
