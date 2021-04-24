9b072b4ec696f57044e0f36d13126d9982e24702
PiDexter <christiaan_wiggers@yahoo.com>
2021-04-24 17:46:50 +0200

    Add abstract Screen

--- src/main/java/mario/view/Screen.java
+++ src/main/java/mario/view/Screen.java
@@ -1,7 +1,10 @@
 package mario.view;
 
 import mario.MainApp;
+import nl.han.ica.oopg.objects.GameObject;
 import nl.han.ica.oopg.view.View;
+
+import java.util.Vector;
 
 public abstract class Screen {
 
@@ -9,9 +12,11 @@ public abstract class Screen {
 
     public Screen(MainApp app) {
         this.app = app;
+        this.app.setView(createView());
+        addObjects();
     }
 
-    public void setupView(View view) {
-        this.app.setView(view);
-    }
+    public abstract void addObjects();
+    public abstract View createView();
+
 }
