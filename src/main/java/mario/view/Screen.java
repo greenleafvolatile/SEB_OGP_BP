8018d6bf0baaf2d7d8bc9b2fb542e41cedc6b1bb
PiDexter <christiaan_wiggers@yahoo.com>
2021-04-24 22:22:09 +0200

    Add render and dispose methods

--- src/main/java/mario/view/Screen.java
+++ src/main/java/mario/view/Screen.java
@@ -1,19 +1,54 @@
 package mario.view;
 
 import mario.MainApp;
+import nl.han.ica.oopg.dashboard.Dashboard;
 import nl.han.ica.oopg.view.View;
 
+/**
+ * The type Screen.
+ */
 public abstract class Screen {
 
+    /**
+     * The App.
+     */
     protected MainApp app;
 
+    /**
+     * Instantiates a new Screen.
+     *
+     * @param app the app
+     */
     public Screen(MainApp app) {
         this.app = app;
+    }
+
+    /**
+     * Add objects.
+     */
+    public abstract void addObjects();
+
+    /**
+     * Create new View instance.
+     *
+     * @return the view
+     */
+    public abstract View createView();
+
+    /**
+     * Render Screen with instance of View and GameObjects.
+     */
+    public void render() {
         this.app.setView(createView());
         this.addObjects();
     }
 
-    public abstract void addObjects();
-    public abstract View createView();
+    /**
+     * Dispose all GameObjects and Dashboards.
+     */
+    public void dispose() {
+        this.app.deleteAllGameOBjects();
+        this.app.deleteAllDashboards();
+    }
 
 }
