a4d9025925fed2fc50f4f68f57b8ea34ebb18ae4
greenleavolatile <daanpol@tutamail.com>
2021-04-26 15:54:11 +0200

    * Added JavaDoc documentation to MapLoader

--- src/main/java/mario/model/map/MapLoader.java
+++ src/main/java/mario/model/map/MapLoader.java
@@ -7,20 +7,23 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
+/**
+ * This class is a utility class for loading an int[][] array
+ * from a file.
+ */
 public final class MapLoader {
 
     private MapLoader(){}
 
-    /***
-     * Used to import a TileMap in CSV format
-     * @return integer 2D array
+    /**
+     * This method loads a map from a file.
+     * @param file a file
+     * @return int[][]
      */
     public static int[][] loadMap(File file) {
-
         int[][] map = null;
 
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) { // Auto close input stream with try-with-resources.
-
             map = new int[getNumberOfLines(file)][];
 
             for (int i = 0; i < map.length; i++) {
@@ -30,30 +33,40 @@ public final class MapLoader {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         return map;
-
     }
 
+    /**
+     * This method loads an empty map from a file.
+     * @return int[][]
+     */
     public static int[][] loadEmptyMap() {
-
         return loadMap(new File(MainApp.MEDIA_URL.concat("/maps/empty.csv")));
     }
 
-    private static int[] toIntArray(String[] array) {
 
+    /**
+     * This method is a helper method for converting a numerical string array
+     * to an integer array.
+     * @param array an array of numerical strings.
+     * @return int[]
+     */
+    private static int[] toIntArray(String[] array) {
         final int[] result = new int[array.length];
 
             for (int i = 0; i < array.length; i++) {
                 result[i] = Integer.parseInt(array[i]);
             }
-
-        return result;
-
+            return result;
     }
 
+    /**
+     * This is a helper method for retrieving the number of lines in a file.
+     * @param file a file
+     * @return int the number of lines.
+     * @throws IOException an IOException.
+     */
     private static int getNumberOfLines(File file) throws IOException {
-
         int numberOfLines = 0;
 
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
@@ -63,9 +76,6 @@ public final class MapLoader {
             }
 
         }
-
         return numberOfLines;
-
     }
-
 }
