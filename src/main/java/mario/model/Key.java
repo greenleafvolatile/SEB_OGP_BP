48a073f20e72c6678a8e7f47d80ce5f2b4669b45
greenleavolatile <daanpol@tutamail.com>
2021-04-27 17:34:07 +0200

    * Refactored Key

--- src/main/java/mario/model/Key.java
+++ src/main/java/mario/model/Key.java
@@ -1,14 +1,25 @@
 package mario.model;
 
+/**
+ * This class represents a key on
+ * a keyboard.
+ *
+ * @author Christiaan Wiggers en Daan Pol
+ * @version 1.0
+ * @since 20-04-2021
+ */
 public class Key {
 
-    private int keyValue;
+    private final int keyValue;
     private boolean pressed;
 
+    /**
+     * The constructor
+     * @param keyValue the UTF-8 value of the key.
+     */
     public Key(int keyValue) {
         this.keyValue = keyValue;
     }
-
 
     public int getKeyValue() {
         return this.keyValue;
@@ -21,5 +32,4 @@ public class Key {
     public boolean isPressed() {
         return pressed;
     }
-
 }
