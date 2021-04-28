9a1a88278ceec3f159abf41212a30c715be9230a
PiDexter <christiaan_wiggers@yahoo.com>
2021-04-28 19:56:26 +0200

    Removed unused methods

--- src/main/java/mario/model/Timer.java
+++ src/main/java/mario/model/Timer.java
@@ -1,12 +1,11 @@
 package mario.model;
 
 /**
- * The type Timer.
+ * The type Timer, based on System nanoTime.
  */
 public class Timer {
 
     private long start;
-    private long end;
 
     /**
      * Start timer.
@@ -14,23 +13,6 @@ public class Timer {
     public void startTimer() {
         this.start = System.nanoTime();
     }
-
-    /**
-     * Stop timer.
-     */
-    public void stopTimer() {
-        this.end = System.nanoTime();
-    }
-
-    /**
-     * Gets total elapsed time when stopTimer is used.
-     *
-     * @return the total elapsed time
-     */
-    public double getTotalElapsedTime() {
-        return (this.end - this.start) / 1_000_000_000.0;
-    }
-
 
     /**
      * Gets elapsed time in seconds.
