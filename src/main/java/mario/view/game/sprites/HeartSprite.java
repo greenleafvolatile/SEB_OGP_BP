48dc92385b8c6180f96d56ff474cea048e344ba9
PiDexter <christiaan_wiggers@yahoo.com>
2021-04-29 19:55:32 +0200

    Add JavaDocs

--- src/main/java/mario/view/game/sprites/HeartSprite.java
+++ src/main/java/mario/view/game/sprites/HeartSprite.java
@@ -4,8 +4,14 @@ import mario.MainApp;
 import mario.ui.Image;
 import nl.han.ica.oopg.objects.Sprite;
 
+/**
+ * The type Heart sprite.
+ */
 public class HeartSprite extends Image {
 
+    /**
+     * Instantiates a new Heart sprite.
+     */
     public HeartSprite() {
         super(new Sprite(MainApp.MEDIA_URL.concat("media/sprites/items/hud_heartFull.png")));
     }
