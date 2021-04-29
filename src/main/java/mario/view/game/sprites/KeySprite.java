e090c3e3a519f5968b3fd68ddb3f24ee9c66d1cc
PiDexter <christiaan_wiggers@yahoo.com>
2021-04-29 19:48:23 +0200

    Add JavaDocs

--- src/main/java/mario/view/game/sprites/KeySprite.java
+++ src/main/java/mario/view/game/sprites/KeySprite.java
@@ -4,9 +4,16 @@ import mario.MainApp;
 import mario.ui.Image;
 import nl.han.ica.oopg.objects.Sprite;
 
+/**
+ * The type Key sprite.
+ */
 public class KeySprite extends Image {
 
+    /**
+     * Instantiates a new Key sprite.
+     */
     public KeySprite() {
         super(new Sprite(MainApp.MEDIA_URL.concat("media/sprites/items/hud_keyYellow.png")));
     }
+
 }
