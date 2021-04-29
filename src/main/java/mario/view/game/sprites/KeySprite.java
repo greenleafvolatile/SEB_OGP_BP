20ef2fa845ec419b70e6f5d5875a6f0ee6a32c8f
PiDexter <christiaan_wiggers@yahoo.com>
2021-04-29 19:47:54 +0200

    Removed unused update method and old import statement

--- src/main/java/mario/view/game/sprites/KeySprite.java
+++ src/main/java/mario/view/game/sprites/KeySprite.java
@@ -3,16 +3,10 @@ package mario.view.game.sprites;
 import mario.MainApp;
 import mario.ui.Image;
 import nl.han.ica.oopg.objects.Sprite;
-import nl.han.ica.oopg.objects.SpriteObject;
 
 public class KeySprite extends Image {
 
     public KeySprite() {
-
         super(new Sprite(MainApp.MEDIA_URL.concat("media/sprites/items/hud_keyYellow.png")));
-    }
-
-    public void update() {
-
     }
 }
